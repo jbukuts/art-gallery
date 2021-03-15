4e3a228b977befc01d445f676c36ad01cef1753f
jbukuts <jbukuts@gmail.com>
2021-03-15 04:32:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,244 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import art from './art.json';
+import $ from 'jquery';
+import GoodLook from './GoodLook.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageList: null,
+      imageFocus: null,
+      showGoodLook: false,
+      isLocal: true
+    };
+
+    // bind some handlers
+    this.removeGoodLook = this.removeGoodLook.bind(this);
+    this.downHandler = this.downHandler.bind(this)
+    this.flipImages = this.flipImages.bind(this)
+  }
+
+  async componentWillMount() {
+    // key listener
+    await this.setState({
+      imageList: await CreateImageCollage(this, this.state.isLocal)
+    });
+
+    console.log(this.state.imageList);
+    window.addEventListener('keydown', this.downHandler);
+  }
+
+  // on pade unmount
+  componentWillUnmount() {
+    // remove key listener
+    window.removeEventListener('keydown', this.downHandler);
+  }
+  
+  // in order to show the good look
+  async showGoodLook(e, wantedImage) {
+    // simply change state
+    // good look is conditional rendered
+    console.log(wantedImage);
+    await this.setState({ 
+      imageFocus: wantedImage,
+      showGoodLook: true
+    });
+  }
+
+  // remove the good look
+  async removeGoodLook() {
+    // fade out and set state to false to unmount
+    $(".in-depth").fadeTo('250ms', 0);
+    $(".info").fadeTo('250ms', 0);
+    $("#backdrop").fadeTo('250ms', 0, () => {
+      this.setState({showGoodLook: false})
+    });
+  }
+
+  // pull images from MET or local
+  async flipImages() {
+    console.log('flip images');
+
+    await $('#photos').fadeTo('1s', 0, async () => {
+      console.log('tes');
+      this.setState({
+        imageList: await CreateImageCollage(this, !this.state.isLocal),
+        isLocal: !this.state.isLocal
+      }, ()=> {$('#photos').fadeIn("slow").delay(1500).fadeTo('1s', 1)});
+    });
+  }
+
+  // handle keypresses
+  downHandler(e) {    
+    // if null ignore
+    if (this.state.imageFocus == null)
+      return;
+
+    // get current index of focued image
+    let index = this.state.imageList.findIndex((i) => i[1].title === this.state.imageFocus.title);
+
+    // figure out what key was pressed
+    // ensure 
+    let newIndex = -1;
+    if (e.key === "ArrowRight")
+      newIndex = (index + 1 > this.state.imageList.length-1) ? 0 : index +1;
+    else if (e.key === "ArrowLeft")
+      newIndex = (index - 1 < 0) ? this.state.imageList.length-1 : index-1;
+    else
+      return;
+    
+    // change state of current image based on keypress
+    this.setState({
+      imageFocus: this.state.imageList[newIndex][1]
+    }) 
+  }
+
+  render() {
+    return (
+      <div style={{width: '85%', margin: 'auto', position: 'relative'}} >
+        {/* if click on image show the detailed view */}
+        {this.state.showGoodLook && <GoodLook focusImage={this.state.imageFocus} handleRemove={this.removeGoodLook}/>}
+        
+        {/* Just a simple header */}
+        <h1 style={{fontSize: '40px', marginBottom: '5px'}}>The Gallery</h1>
+        <p style={{marginTop: '0px'}}>
+          This is just a collection of art I like.<br/>
+          Thought it'd be nice to see them all in one place.<br/>
+          I do not own any of them of course.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button onClick={this.flipImages}>Pull from MET</button>
+
+        <br/>
+        <br/>
+
+        {/* the actual image collage */}
+        <div id="photos">
+          {this.state.imageList && this.state.imageList.map(x => x[0])}
+        </div>
+
+
+        <div id="footer" style={{position: 'relative', width: '100%', height: '100px', marginTop: '25px'}}>
+          <div style={{width: '50%', position: 'absolute', bottom: '0', left: '0', display: 'flex', marginBottom: '5px'}}>
+            <img style={{width: '100px' , height: '100px'}} src={`${process.env.PUBLIC_URL}/assets/MetLogo.svg`} alt="Thank you to the MET" height={'100'} width={'100'}/>
+            
+          </div>
+
+          <div style={{width: '50%', position: 'absolute', bottom: '0', right: '0', textAlign: 'right'}}>
+            <p style={{marginBottom: '0px', fontSize: '14px'}}>Jake Bukuts 2021</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+// create the image collage
+async function CreateImageCollage(thing, isLocal) {
+
+  // only one work per artist
+  var clonedArray = JSON.parse(JSON.stringify(art))
+
+  const filteredArr = shuffleArray(clonedArray).reduce((acc, current) => {
+    const x = acc.find(item => item.artist === current.artist);
+      return !x ? acc.concat([current]) : acc;
+  }, []).map(x => {x.file_name = `${process.env.PUBLIC_URL}/assets/art/${x.file_name}`; return x});
+  
+  const imageList =  isLocal ? filteredArr : await getRandomArtList();
+  console.log(imageList);
+  console.log(art);
+
+  return imageList.map(
+    (x,i) => {
+      return [createSingleImage(x,i,thing), x]
+    }
   );
+}
+
+// creates a single image to display in list
+function createSingleImage(x,i, thing) {
+  return (
+    <div className={"work"} key={i}>
+        <div className={"meta"}>
+          <div className={"icon"}></div>
+          <div className={"file-name"}>
+            <a href={`https://www.google.com/search?q=${x.title} ${x.artist}`} target="_blank" rel="noreferrer">{x.title.replaceAll(' ', '_').toLowerCase()}.jpg</a>
+          </div>
+        </div>
+        
+        <div style={{display: 'flex'}}>
+          <div className={"banner-left"}  style={{height: '55px', width: '180px'}}></div>
+          <div className={"banner-mid"}   style={{height: '55px', width: 'calc(100% - 331px)'}}></div>
+          <div className={"banner-right"} style={{height: '55px', width: '151px'}}></div>
+        </div>
+        <img src={x.file_name} alt="" onMouseEnter={showWorkInfo} onMouseLeave={removeWorkInfo} onClick={(e)=> thing.showGoodLook(e,x)}/>
+    </div>
+  )
+}
+
+// pull random image data from the MET 
+async function getRandomArtList() {
+  let url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=painting';
+  
+  return await fetch(url)
+    .then(r => r.json())
+    .then(async (r) => {
+      const total = r.total;
+      let works = [];
+
+      for (let i = 0; i < 15; ++i) {
+        let id = Math.floor(Math.random() * (total-1));
+        await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${r.objectIDs[id]}`)
+          .then(r => r.json())
+          .then(r => {
+            r['date'] = (r.artistDisplayName) !== '' ? r.objectDate : 'Unknown';
+            r['file_name'] = r.primaryImage;
+            r['artist'] = (r.artistDisplayName) !== '' ? r.artistDisplayName : 'Unknown';
+            r['comments'] = (r.medium) !== '' ? r.medium : 'N/A';
+            r['title'] = r.title.length > 50 ? `${r.title.substring(0,50)}` : r.title;
+            works.push(r);
+        });
+      }
+
+      return works;
+    });
+}
+
+// grey out all except for one
+function showWorkInfo(e) {
+  let invert = [...document.querySelectorAll('.work')].filter(x => x !== e.target.parentNode);
+  invert.forEach(x => {
+    x.children[2].style.filter = 'grayscale(100%) blur(0px)';
+  })
+}
+
+// reset the colors of all images
+function removeWorkInfo(e) {
+  let invert = [...document.querySelectorAll('.work')].filter(x => x !== e.target.parentNode);
+  invert.forEach(x => {
+    x.children[2].style.filter = 'grayscale(0%) blur(0px)';
+  })
+}
+
+// helper to shuffle array
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 export default App;
---
+++ src/GoodLook.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import './App.css';
+import $ from 'jquery';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import Draggable from 'react-draggable';
+
+class GoodLook extends React.Component {
+  
+    // on mount fade the components in
+    componentDidMount() {
+      $("#backdrop").fadeTo('250ms', 1);
+      $(".in-depth").fadeTo('250ms', 1);
+      $(".info").fadeTo('250ms', 1);
+    }
+  
+    // show the detailed view
+    render() {
+  
+      // stuff to display
+      let fileName = `${this.props.focusImage.title.replaceAll(' ', '_').toLowerCase()}.jpg`;
+      let d = new Date();
+      let months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemver', 'December'];
+      let timeString = d.getHours() > 12 ? `${d.getHours()-12}:${d.getMinutes()} PM` : `${d.getHours()}:${d.getMinutes()} AM`;
+      let dateString = `Modified: ${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()} at ${timeString}`;
+  
+      // triangle icon
+      let triangle = (r) => {return (<svg height="10" width="10" viewBox="0 0 10 10">
+        <polygon points=".2 .2,9.8 .2,4.8 9.8" className={"triangle-icon"} transform={`rotate(${r ? 270 : 0},5,5)`}/>
+      </svg>)};
+  
+      // seperator between sections
+      let sep = <hr style={{paddingRight: '-3px', paddingLeft: '-3px', color: '#9f9f9f'}}></hr>;
+  
+      // creates the header
+      let header = (r, title) => <span>
+        <span>{triangle(r)}</span>
+        <span style={{display: 'inline-block', verticalAlign: 'middle'}}>&nbsp;&nbsp;{title}:</span>
+      </span>
+  
+      // image data
+      const imageData = this.props.focusImage;
+  
+      // sections to creates
+      let sections = [
+        {
+          name : "General", 
+          properties: [["Name",imageData.title],["Creator", imageData.artist],["Date", imageData.date]]
+        },
+        {
+          name : "More Info", 
+          properties: [["Medium",imageData.classification],["Dimensions", imageData.dimensions]]
+        },
+        {
+          name : "Name & Extension"
+        },
+        {
+          name : "Comments", 
+          properties: [["",imageData.comments]]
+        },
+        {
+          name : "Open with"
+        },
+        {
+          name : "Preview"
+        },
+        {
+          name : "Sharing & Permissions"
+        }
+      ]
+  
+      // add properties to section as needed
+      let addProperties = (properties) => {
+        return properties.map(x => {
+          return ( x[1] &&
+            <p style={{marginBottom: '2px', marginTop: '2px', marginLeft: "30px"}}>{x[0].length > 0 ? `${x[0]}: ` : ''}{x[1]}</p>
+          )
+        });
+      }
+  
+      // create the sections from the array
+      let createSections = (sections) => {
+        return sections.map(x => {
+
+          const areThere = x.properties? x.properties.reduce((acc, cur) => { return !acc ? acc : (cur[1] ? true : false)}, true) : false;
+
+          return ( 
+            <div>
+              {sep}
+              {header(!areThere, x.name)}
+              {x.properties != null && addProperties(x.properties)}
+            </div>
+          )
+        });
+      }
+
+      return (
+        <div>
+          {/* This will blur the background */}
+          <div id="backdrop" onClick={this.props.handleRemove}></div>
+  
+          {/* The actual full view of the image */}
+          <Draggable defaultPosition={{x: 100, y: 50}} cancel=".react-transform-component">
+            <div className={"in-depth"}>     
+              <div className={"banner-mid"}   style={{height: '25px', width: '100%', textAlign: 'center', fontSize: '10px'}}>
+                <p style={{paddingTop: '8px', margin: '0px'}}>{fileName}</p>
+  
+                <div style={{position: 'relative', display: 'flex', marginTop: '-16px'}}>
+                  <div className={'top-button'} style={{backgroundColor: '#FC5753', marginLeft: '5px', border: '1px solid #E03e3b'}}></div>
+                  <div className={'top-button'} style={{backgroundColor: '#FDBC40', border: '1px solid #dd9b23'}}></div>
+                  <div className={'top-button'} style={{backgroundColor: '#33C748', border: '1px solid #24a733'}}></div>
+                </div>
+              </div>
+  
+              {/* allow for zoom on the image */}
+              <TransformWrapper>
+                <TransformComponent>
+                  <img src={this.props.focusImage.file_name} alt="" style={{marginBottom: '-6px'}}/>
+                </TransformComponent>
+              </TransformWrapper>
+            </div>
+          </Draggable>
+  
+          {/* the info window on the image */}
+          <Draggable defaultPosition={{x: window.innerWidth-500, y: 500}}>
+            <div className={'info'} style={{borderCollapse: 'separate'}}>
+              <div className={"banner-mid"}   style={{height: '25px', width: '100%', textAlign: 'center', fontSize: '10px', position: 'relative'}}>
+                <div className={"icon"} style={{margin: '0px', position: 'absolute', top: '50%', transform: 'translateY(-50%)'}}></div>
+                <div>Image Info</div>
+              </div>
+            
+              <div style={{paddingRight: '7px', paddingLeft: '7px', marginTop: '10px', fontSize: '11px', wordWrap: 'break-word'}}>
+                {/* header of the window*/}
+                <p style={{fontSize: '14px', fontWeight: 'bold', marginBottom: '5px'}}>{fileName}</p>
+                <p style={{marginTop: '0px'}}>{dateString}</p>
+                
+                {createSections(sections)}
+              </div>
+            </div>
+          </Draggable>
+        </div> 
+      )
+    }
+  }
+
+  export default GoodLook;
